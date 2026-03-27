1adbce8c92ae6a4dfac30f1fd653ebdcb3f57cb5
boris-dotv <1322553126@qq.com>
2026-03-27 14:30:02 +0000

    updated

--- sglang_sala_cp/sglang/_version.py
+++ sglang_sala_cp/sglang/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.0.dev33+g0dbda43c2.d20260327'
-__version_tuple__ = version_tuple = (0, 0, 0, 'dev33', 'g0dbda43c2.d20260327')
+__version__ = version = '0.0.0.dev52+g9241d8813.d20260327'
+__version_tuple__ = version_tuple = (0, 0, 0, 'dev52', 'g9241d8813.d20260327')
 
-__commit_id__ = commit_id = 'g0dbda43c2'
+__commit_id__ = commit_id = 'g9241d8813'
