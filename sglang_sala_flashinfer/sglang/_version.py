fcaeeb7f894dd927aba016f3660837a2c52d1862
boris-dotv <1322553126@qq.com>
2026-04-02 14:57:26 +0000

    test done

--- sglang_sala_flashinfer/sglang/_version.py
+++ sglang_sala_flashinfer/sglang/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.0.dev148+gbb859c778.d20260401'
-__version_tuple__ = version_tuple = (0, 0, 0, 'dev148', 'gbb859c778.d20260401')
+__version__ = version = '0.0.0.dev154+g818611a40.d20260402'
+__version_tuple__ = version_tuple = (0, 0, 0, 'dev154', 'g818611a40.d20260402')
 
-__commit_id__ = commit_id = 'gbb859c778'
+__commit_id__ = commit_id = 'g818611a40'
--- sglang_sala_profiling/sglang/_version.py
+++ sglang_sala_profiling/sglang/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.0.dev125+gac9ad70c2.d20260401'
-__version_tuple__ = version_tuple = (0, 0, 0, 'dev125', 'gac9ad70c2.d20260401')
+__version__ = version = '0.0.0.dev156+gda11994ad.d20260402'
+__version_tuple__ = version_tuple = (0, 0, 0, 'dev156', 'gda11994ad.d20260402')
 
-__commit_id__ = commit_id = 'gac9ad70c2'
+__commit_id__ = commit_id = 'gda11994ad'
