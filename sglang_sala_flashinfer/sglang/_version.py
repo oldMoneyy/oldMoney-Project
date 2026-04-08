6fba92007372018db1b34ee88fc1f695022a8d6b
boris-dotv <1322553126@qq.com>
2026-04-08 10:45:07 +0000

    triton tested

--- sglang_sala_flashinfer/sglang/_version.py
+++ sglang_sala_flashinfer/sglang/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.0.dev194+g5ac29a07b.d20260408'
-__version_tuple__ = version_tuple = (0, 0, 0, 'dev194', 'g5ac29a07b.d20260408')
+__version__ = version = '0.0.0.dev204+g611074d90.d20260408'
+__version_tuple__ = version_tuple = (0, 0, 0, 'dev204', 'g611074d90.d20260408')
 
-__commit_id__ = commit_id = 'g5ac29a07b'
+__commit_id__ = commit_id = 'g611074d90'
